c54218045261c69ec068fc7e737f0fb4809ded81
Jaroslaw Romanczuk <szamotulok@gmail.com>
2025-05-21 23:54:26 +0200

    First commit

---
+++ src/main/java/wmi/amu/edu/pl/pri/PriApplication.java
@@ -0,0 +1,13 @@
+package wmi.amu.edu.pl.pri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PriApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wmi/amu/edu/pl/pri/PriApplicationTests.java
@@ -0,0 +1,13 @@
+package wmi.amu.edu.pl.pri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
