9b8110de1512f63f7ef4a1b610656213ae16ceea
Jarosław Romańczuk <53583288+jarrobots@users.noreply.github.com>
2025-10-24 16:44:33 +0200

    Remove all files from deploy branch

--- src/main/java/wmi/amu/edu/pl/pri/repositories/FileContentRepo.java
+++
@@ -1,8 +0,0 @@
-package wmi.amu.edu.pl.pri.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import wmi.amu.edu.pl.pri.models.FileContentModel;
-
-
-public interface FileContentRepo extends JpaRepository<FileContentModel, Long> {
-}
--- src/main/java/wmi/amu/edu/pl/pri/repositories/SupervisorRepo.java
+++
@@ -1,7 +0,0 @@
-package wmi.amu.edu.pl.pri.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import wmi.amu.edu.pl.pri.models.pri.SupervisorModel;
-
-public interface SupervisorRepo extends JpaRepository<SupervisorModel, Long > {
-}
